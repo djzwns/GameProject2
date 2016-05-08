b4b7ca67fd70f36917ef129931b59f433513989e
djzwns <hjjshg@game.hoseo.edu>
2016-05-08 23:57:47 +0900

    플레이어를 컨트롤하는 스크립트로 공격. 스킬 등을 다루는 클래스

---
+++ Assets/Scripts/Unit/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    private UnitEntity player;
+    private float attackTime;
+
+	// Use this for initialization
+	void Awake () {
+        player = Player.Instance;
+        attackTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        attackTime += Time.deltaTime;
+    }
+
+    // 충돌해 있을 때 공격 시간 마다 공격
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        if (attackTime >= player.AttackSpeed)
+        {
+            attackTime = 0;
+            player.Attack(player);
+        }
+    }
+}
