4636851073f3b1eb19dc2d4023ebbafe9676e3e7
djzwns <hjjshg@game.hoseo.edu>
2016-05-08 23:58:15 +0900

    적의 움직임을 다루는 스크립트

---
+++ Assets/Scripts/Unit/Enemy/EnemyMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+    // 리지드바디 사용
+    private Rigidbody2D rigid2D;
+
+    void Awake()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (rigid2D.velocity.y == 0)
+            rigid2D.velocity = -(Vector2.right * 2.0f * 0.1f) + rigid2D.velocity;
+    }
+}
