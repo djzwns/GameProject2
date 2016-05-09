b21f99f119f4f4f4b290c09e4d5fcb4b8d381faf
djzwns <hjjshg@game.hoseo.edu>
2016-05-09 19:19:56 +0900

    플레이어와 충돌 시 공격하게 만듬

---
+++ Assets/Scripts/Unit/Enemy/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : UnitEntity
+{
+    public override void TakeDamage(int amount)
+    {
+        base.TakeDamage(amount);
+    }
+
+    public override void Attack(UnitEntity unit)
+    {
+        base.Attack(unit);
+    }
+}
---
+++ Assets/Scripts/Unit/Enemy/EnemyController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+    // 적 정보를 담고 있음 private 이지만 인스펙터창에 노출
+    [SerializeField]
+    private Enemy enemy;
+    // 플레이어 정보를 받아옴
+    private Player player;
+    // 공격 가능한 시간
+    private float attackTime = 0f;
+
+	// Use this for initialization
+	void Awake () {
+        enemy = ScriptableObject.CreateInstance<Enemy>() as Enemy;
+        player = Player.Instance;
+	}
+
+    void Update()
+    {
+        attackTime += Time.deltaTime;
+        if (enemy.IsDead())
+            gameObject.SetActive(false);
+    }
+
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag.Equals("Player"))
+        {
+            if (attackTime >= enemy.AttackSpeed)
+            {
+                if (player.Evasion <= Random.Range(1, 100))
+                {
+                    attackTime = 0;
+                    enemy.Attack(player);
+                }
+                else
+                {
+                    Debug.Log("플레이어 회피");
+                }
+            }
+        }
+    }
+
+    // 적 정보 보내줌
+    public UnitEntity Enemy { get { return enemy; }}
+}
